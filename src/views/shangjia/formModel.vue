22cdc664b1b8129c1c5d052ac5b3205ec8ffdbf3
mingyuanli123 <122319547+mingyuanli123@users.noreply.github.com>
2024-03-03 18:38:20 -0600

    Add files via upload

---
+++ src/views/shangjia/formModel.vue
@@ -0,0 +1,635 @@
+<template>
+	<div>
+		<el-dialog v-model="formVisible" :title="formTitle" width="80%" destroy-on-close :fullscreen='false'>
+			<el-form class="formModel_form" ref="formRef" :model="form" label-width="$template2.back.add.form.base.labelWidth" :rules="rules">
+				<el-row>
+					<el-col :span="12">
+						<el-form-item label="商家账号" prop="shangjiazhanghao">
+							<el-input class="list_inp" v-model="form.shangjiazhanghao" placeholder="商家账号"
+								type="text" :readonly="!isAdd||disabledForm.shangjiazhanghao?true:false" />
+						</el-form-item>
+					</el-col>
+					<el-col :span="12">
+						<el-form-item label="密码" prop="mima">
+							<el-input class="list_inp" v-model="form.mima" placeholder="密码"
+								type="password" :readonly="!isAdd||disabledForm.mima?true:false" />
+						</el-form-item>
+					</el-col>
+					<el-col :span="12">
+						<el-form-item label="商家名称" prop="shangjiamingcheng">
+							<el-input class="list_inp" v-model="form.shangjiamingcheng" placeholder="商家名称"
+								 type="text" 								:readonly="!isAdd||disabledForm.shangjiamingcheng?true:false" />
+						</el-form-item>
+					</el-col>
+
+					<el-col :span="12">
+						<el-form-item label="性别" prop="xingbie">
+							<el-select
+								class="list_sel"
+								:disabled="!isAdd||disabledForm.xingbie?true:false"
+								v-model="form.xingbie" 
+								placeholder="请选择性别"
+								>
+								<el-option v-for="(item,index) in xingbieLists" :label="item"
+									:value="item"
+									>
+								</el-option>
+							</el-select>
+						</el-form-item>
+					</el-col>
+					<el-col :span="12">
+						<el-form-item label="店铺图片" prop="dianputupian">
+							<uploads
+								:disabled="!isAdd||disabledForm.dianputupian?true:false"
+								action="file/upload" 
+								tip="请上传店铺图片" 
+								:limit="3" 
+								style="width: 100%;text-align: left;"
+								:fileUrls="form.dianputupian?form.dianputupian:''" 
+								@change="dianputupianUploadSuccess">
+							</uploads>
+						</el-form-item>
+					</el-col>
+					<el-col :span="12">
+						<el-form-item label="手机号码" prop="shoujihaoma">
+							<el-input class="list_inp" v-model="form.shoujihaoma" placeholder="手机号码"
+								 type="text" 								:readonly="!isAdd||disabledForm.shoujihaoma?true:false" />
+						</el-form-item>
+					</el-col>
+
+					<el-col :span="12">
+						<el-form-item label="联系人" prop="lianxiren">
+							<el-input class="list_inp" v-model="form.lianxiren" placeholder="联系人"
+								 type="text" 								:readonly="!isAdd||disabledForm.lianxiren?true:false" />
+						</el-form-item>
+					</el-col>
+
+					<el-col :span="12">
+						<el-form-item label="经营地址" prop="jingyingdizhi">
+							<el-input class="list_inp" v-model="form.jingyingdizhi" placeholder="经营地址"
+								 type="text" 								:readonly="!isAdd||disabledForm.jingyingdizhi?true:false" />
+						</el-form-item>
+					</el-col>
+
+					<el-col :span="12">
+						<el-form-item label="主要经营" prop="zhuyaojingying">
+							<el-input v-model="form.zhuyaojingying" placeholder="主要经营" type="textarea"
+							:readonly="!isAdd||disabledForm.zhuyaojingying?true:false"
+							/>
+						</el-form-item>
+					</el-col>
+				</el-row>
+			</el-form>
+			<template #footer v-if="isAdd||type=='logistics'||type=='reply'">
+				<span class="formModel_btn_box">
+					<el-button class="formModel_cancel" @click="closeClick">取消</el-button>
+					<el-button class="formModel_confirm" type="primary" @click="save"
+						>
+						提交
+					</el-button>
+				</span>
+			</template>
+		</el-dialog>
+	</div>
+</template>
+<script setup>
+	import {
+		reactive,
+		ref,
+		getCurrentInstance,
+		nextTick,
+		computed,
+		defineEmits
+	} from 'vue'
+	const context = getCurrentInstance()?.appContext.config.globalProperties;	
+	const emit = defineEmits(['formModelChange'])
+	//基础信息
+	const tableName = 'shangjia'
+	const formName = '商家'
+	//基础信息
+	//form表单
+	const form = ref({})
+	const disabledForm = ref({
+		shangjiazhanghao : false,
+		mima : false,
+		shangjiamingcheng : false,
+		xingbie : false,
+		dianputupian : false,
+		shoujihaoma : false,
+		lianxiren : false,
+		jingyingdizhi : false,
+		zhuyaojingying : false,
+	})
+	const formVisible = ref(false)
+	const isAdd = ref(false)
+	const formTitle = ref('')
+	//表单验证
+	//匹配整数
+	const validateIntNumber = (rule, value, callback) => {
+		if (!value) {
+			callback();
+		} else if (!context?.$toolUtil.isIntNumer(value)) {
+			callback(new Error("请输入整数"));
+		} else {
+			callback();
+		}
+	}
+	//匹配数字
+	const validateNumber = (rule, value, callback) => {
+		if(!value){
+			callback();
+		} else if (!context?.$toolUtil.isNumber(value)) {
+			callback(new Error("请输入数字"));
+		} else {
+			callback();
+		}
+	}
+	//匹配手机号码
+	const validateMobile = (rule, value, callback) => {
+		if(!value){
+			callback();
+		} else if (!context?.$toolUtil.isMobile(value)) {
+			callback(new Error("请输入正确的手机号码"));
+		} else {
+			callback();
+		}
+	}
+	//匹配电话号码
+	const validatePhone = (rule, value, callback) => {
+		if(!value){
+			callback();
+		} else if (!context?.$toolUtil.isPhone(value)) {
+			callback(new Error("请输入正确的电话号码"));
+		} else {
+			callback();
+		}
+	}
+	//匹配邮箱
+	const validateEmail = (rule, value, callback) => {
+		if(!value){
+			callback();
+		} else if (!context?.$toolUtil.isEmail(value)) {
+			callback(new Error("请输入正确的邮箱地址"));
+		} else {
+			callback();
+		}
+	}
+	//匹配身份证
+	const validateIdCard = (rule, value, callback) => {
+		if(!value){
+			callback();
+		} else if (!context?.$toolUtil.checkIdCard(value)) {
+			callback(new Error("请输入正确的身份证号码"));
+		} else {
+			callback();
+		}
+	}
+	//匹配网站地址
+	const validateUrl = (rule, value, callback) => {
+		if(!value){
+			callback();
+		} else if (!context?.$toolUtil.isURL(value)) {
+			callback(new Error("请输入正确的URL地址"));
+		} else {
+			callback();
+		}
+	}
+	const rules = ref({
+		shangjiazhanghao: [
+			{required: true,message: '请输入',trigger: 'blur'}, 
+		],
+		mima: [
+			{required: true,message: '请输入',trigger: 'blur'}, 
+		],
+		shangjiamingcheng: [
+			{required: true,message: '请输入',trigger: 'blur'}, 
+		],
+		xingbie: [
+		],
+		dianputupian: [
+		],
+		shoujihaoma: [
+			{required: true,message: '请输入',trigger: 'blur'}, 
+			{ validator: validateMobile, trigger: 'blur' },
+		],
+		lianxiren: [
+		],
+		jingyingdizhi: [
+		],
+		zhuyaojingying: [
+		],
+	})
+	//表单验证
+	
+	const formRef = ref(null)
+	const id = ref(0)
+	const type = ref('')
+	//性别列表
+	const xingbieLists = ref([])
+	//店铺图片上传回调
+	const dianputupianUploadSuccess=(e)=>{
+		form.value.dianputupian = e
+	}
+	//methods
+
+	//获取唯一标识
+	const getUUID =()=> {
+      return new Date().getTime();
+    }
+	//重置
+	const resetForm = () => {
+		form.value = {
+			shangjiazhanghao: '',
+			mima: '',
+			shangjiamingcheng: '',
+			xingbie: '男',
+			dianputupian: '',
+			shoujihaoma: '',
+			lianxiren: '',
+			jingyingdizhi: '',
+			zhuyaojingying: '',
+		}
+	}
+	//获取info
+	const getInfo = ()=>{
+		context?.$http({
+			url: `${tableName}/info/${id.value}`,
+			method: 'get'
+		}).then(res => {
+			let reg=new RegExp('../../../file','g')
+			form.value = res.data.data
+			formVisible.value = true
+		})
+	}
+	const crossRow = ref('')
+	const crossTable = ref('')
+	const crossTips = ref('')
+	const crossColumnName = ref('')
+	const crossColumnValue = ref('')
+	//初始化
+	const init=(formId=null,formType='add',formNames='',row=null,table=null,statusColumnName=null,tips=null,statusColumnValue=null)=>{
+		resetForm()
+		if(formId){
+			id.value = formId
+			type.value = formType
+		}
+		if(formType == 'add'){
+			isAdd.value = true
+			formTitle.value = '新增' + formName
+			formVisible.value = true
+		}else if(formType == 'info'){
+			isAdd.value = false
+			formTitle.value = '查看' + formName
+			getInfo()
+		}else if(formType == 'edit'){
+			isAdd.value = true
+			formTitle.value = '修改' + formName
+			getInfo()
+		}
+		else if(formType == 'cross'){
+			isAdd.value = true
+			formTitle.value = formNames
+			// getInfo()
+			for(let x in row){
+				if(x=='shangjiazhanghao'){
+					form.value.shangjiazhanghao = row[x];
+					disabledForm.value.shangjiazhanghao = true;
+					continue;
+				}
+				if(x=='mima'){
+					form.value.mima = row[x];
+					disabledForm.value.mima = true;
+					continue;
+				}
+				if(x=='shangjiamingcheng'){
+					form.value.shangjiamingcheng = row[x];
+					disabledForm.value.shangjiamingcheng = true;
+					continue;
+				}
+				if(x=='xingbie'){
+					form.value.xingbie = row[x];
+					disabledForm.value.xingbie = true;
+					continue;
+				}
+				if(x=='dianputupian'){
+					form.value.dianputupian = row[x];
+					disabledForm.value.dianputupian = true;
+					continue;
+				}
+				if(x=='shoujihaoma'){
+					form.value.shoujihaoma = row[x];
+					disabledForm.value.shoujihaoma = true;
+					continue;
+				}
+				if(x=='lianxiren'){
+					form.value.lianxiren = row[x];
+					disabledForm.value.lianxiren = true;
+					continue;
+				}
+				if(x=='jingyingdizhi'){
+					form.value.jingyingdizhi = row[x];
+					disabledForm.value.jingyingdizhi = true;
+					continue;
+				}
+				if(x=='zhuyaojingying'){
+					form.value.zhuyaojingying = row[x];
+					disabledForm.value.zhuyaojingying = true;
+					continue;
+				}
+			}
+			if(row){
+				crossRow.value = row
+			}
+			if(table){
+				crossTable.value = table
+			}
+			if(tips){
+				crossTips.value = tips
+			}
+			if(statusColumnName){
+				crossColumnName.value = statusColumnName
+			}
+			if(statusColumnValue){
+				crossColumnValue.value = statusColumnValue
+			}
+			form.value.xingbie='男'
+			formVisible.value = true
+		}
+
+		context?.$http({
+			url: `${context?.$toolUtil.storageGet('sessionTable')}/session`,
+			method: 'get'
+		}).then(res => {
+			var json = res.data.data
+		})
+		xingbieLists.value = "男,女".split(',')
+	}
+	//初始化
+	//声明父级调用
+	defineExpose({
+		init
+	})
+	//关闭
+	const closeClick = () => {
+		formVisible.value = false
+	}
+	//富文本
+	const editorChange = (e,name) =>{
+		form.value[name] = e
+	}
+	//提交
+	const save=()=>{
+		if(form.value.dianputupian!=null) {
+			form.value.dianputupian = form.value.dianputupian.replace(new RegExp(context?.$config.url,"g"),"");
+		}
+		var table = crossTable.value
+		var objcross = JSON.parse(JSON.stringify(crossRow.value))
+		let crossUserId = ''
+		let crossRefId = ''
+		let crossOptNum = ''
+		if(type.value == 'cross'){
+			if(crossColumnName.value!=''){
+				if(!crossColumnName.value.startsWith('[')){
+					for(let o in objcross){
+						if(o == crossColumnName.value){
+							objcross[o] = crossColumnValue.value
+						}
+					}
+					//修改跨表数据
+					changeCrossData(objcross)
+				}else{
+					crossUserId = context?.$toolUtil.storageGet('userid')
+					crossRefId = objcross['id']
+					crossOptNum = crossColumnName.value.replace(/\[/,"").replace(/\]/,"")
+				}
+			}
+		}
+		formRef.value.validate((valid)=>{
+			if(valid){
+				if(crossUserId&&crossRefId){
+					form.value.crossuserid = crossUserId
+					form.value.crossrefid = crossRefId
+					let params = {
+						page: 1,
+						limit: 1000, 
+						crossuserid:form.value.crossuserid,
+						crossrefid:form.value.crossrefid,
+					}
+					context?.$http({
+						url: `${tableName}/page`,
+						method: 'get', 
+						params: params 
+					}).then(res=>{
+						if(res.data.data.total>=crossOptNum){
+							context?.$toolUtil.message(`${crossTips.value}`,'error')
+							return false
+						}else{
+							context?.$http({
+								url: `${tableName}/${!form.value.id ? "save" : "update"}`,
+								method: 'post', 
+								data: form.value 
+							}).then(res=>{
+								context?.$toolUtil.message(`操作成功`,'success',()=>{
+									formVisible.value = false
+									emit('formModelChange')
+								})
+							})
+						}
+					})
+				}else{
+					context?.$http({
+						url: `${tableName}/${!form.value.id ? "save" : "update"}`,
+						method: 'post', 
+						data: form.value 
+					}).then(res=>{
+						context?.$toolUtil.message(`操作成功`,'success',()=>{
+							formVisible.value = false
+							emit('formModelChange')
+						})
+					})
+				}
+			}
+		})
+	}
+	//修改跨表数据
+	const changeCrossData=(row)=>{
+		context?.$http({
+			url: `${crossTable.value}/update`,
+			method: 'post',
+			data: row
+		}).then(res=>{})
+	}
+</script>
+<style lang="scss" scoped>
+	// 表单
+	.formModel_form{
+		border-radius: 6px;
+		padding: 30px;
+		box-shadow: 0 0px 0px rgba(85, 255, 255, 0.5);
+		// form item
+		:deep(.el-form-item) {
+			border: 1px solid #ddd;
+			margin: 0;
+			display: flex;
+			//label
+			.el-form-item__label {
+			 border: 1px solid #ddd;
+			 padding: 0;
+			 background: rgba(236,236,236,1);
+			 display: block;
+			 width: 120px;
+			 border-width: 0 2px 0 0;
+			 text-align: center;
+			}
+			// 内容盒子
+			.el-form-item__content {
+				padding: 0 20px;
+				display: flex;
+				width: calc(100% - 120px);
+				justify-content: flex-start;
+				align-items: center;
+				flex-wrap: wrap;
+				// 输入框
+				.list_inp {
+					border: 1px solid #ddd;
+					box-shadow: 0 0 0px rgba(85, 255, 255, 0.5);
+					padding: 0 10px;
+					width: 100%;
+					line-height: 36px;
+					box-sizing: border-box;
+					height: 36px;
+					//去掉默认样式
+					.el-input__wrapper{
+						border: none;
+						box-shadow: none;
+						background: none;
+						border-radius: 0;
+						height: 100%;
+						padding: 0;
+					}
+					.is-focus {
+						box-shadow: none !important;
+					}
+				}
+				// 下拉框
+				.list_sel {
+					border: 1px solid #ddd;
+					border-radius: 0;
+					box-shadow: 0 0 0px rgba(85, 255, 255, 0.5);
+					padding: 0 10px;
+					width: 100%;
+					line-height: 36px;
+					box-sizing: border-box;
+					//去掉默认样式
+					.select-trigger{
+						height: 100%;
+						.el-input{
+							height: 100%;
+							.el-input__wrapper{
+								border: none;
+								box-shadow: none;
+								background: none;
+								border-radius: 0;
+								height: 100%;
+								padding: 0;
+							}
+							.is-focus {
+								box-shadow: none !important;
+							}
+						}
+					}
+				}
+				// 长文本
+				.el-textarea__inner {
+					border: 1px solid #ddd;
+					border-radius: 0;
+					padding: 12px;
+					outline: none;
+					color: #333;
+					width: 100%;
+					font-size: 14px;
+					min-height: 120px;
+				}
+				//图片上传样式
+				.el-upload-list  {
+					//提示语
+					.el-upload__tip {
+						margin: 7px 0 0;
+						color: #999;
+						display: flex;
+						font-size: 12px;
+						justify-content: flex-start;
+						align-items: center;
+					}
+					//外部盒子
+					.el-upload--picture-card {
+						border: 1px dashed #000;
+						cursor: pointer;
+						background-color: #fff;
+						border-radius: 8px;
+						width: 100px;
+						line-height: 110px;
+						text-align: center;
+						height: 100px;
+						//图标
+						.el-icon{
+							color: #000;
+							font-size: 32px;
+						}
+					}
+					.el-upload-list__item {
+						border: 1px dashed #000;
+						cursor: pointer;
+						background-color: #fff;
+						border-radius: 8px;
+						width: 100px;
+						line-height: 110px;
+						text-align: center;
+						height: 100px;
+					}
+				}
+			}
+		}
+	}
+	// 按钮盒子
+	.formModel_btn_box {
+		padding: 40px 0 0;
+		display: flex;
+		width: 100%;
+		justify-content: center;
+		align-items: center;
+		.formModel_cancel {
+			border: 0;
+			cursor: pointer;
+			border-radius: 4px;
+			padding: 0 24px;
+			margin: 0 10px 0 0;
+			outline: none;
+			color: #fff;
+			background: rgba(27, 78, 133, 1);
+			width: auto;
+			font-size: 14px;
+			height: 32px;
+		}
+		.formModel_cancel:hover {
+			background: rgba(27, 78, 133, 0.5);
+		}
+		
+		.formModel_confirm {
+			border: 0;
+			cursor: pointer;
+			border-radius: 4px;
+			padding: 0 24px;
+			margin: 0 10px 0 0;
+			outline: none;
+			color: #fff;
+			background: rgba(241, 49, 100, 1);
+			width: auto;
+			font-size: 14px;
+			height: 32px;
+		}
+		.formModel_confirm:hover {
+			background: rgba(241, 49, 100, .5);
+		}
+	}
+</style>
